f0681a417444b72b9296ee2a0c434757c8018adb
Elena Carey <theelenacarey@gmail.com>
2022-06-13 11:36:56 -0500

    Added a stop for passing null.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
-
-
+function redAlert() {
+    function undo(){
+        $('#zip').removeClass('has-background-danger');
+    }
+    $('#zip').addClass('has-background-danger');
+    setTimeout(undo, 500);
+}
 
 $(document).ready(() => {
     $('#search-btn').on('click', () => {
         zipEntry = zip.value
-        $('#home').hide();
-        $('#search-page').show();
-        console.log(zipEntry);
+        if (zipEntry){
+            $('#home').hide();
+            $('#search-page').show();
+            console.log(zipEntry);
+            zip.value = "";
+        } else {
+            redAlert();
+        }
     })
 
     $('#find-me-btn').on('click', () => {
@@ -18,6 +28,11 @@ $(document).ready(() => {
         $('#search-page').hide();
         $('#home').show();
     })
+    $('#show-img').on('click', () => {
+        dayEntry = datepicker.value;
+        console.log(dayEntry);
+    }
+    )
 
 });
 

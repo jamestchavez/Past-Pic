b1c36413aa4c163b6ac3511bca683d0510dd4c6c
Elena Carey <theelenacarey@gmail.com>
2022-06-13 10:45:51 -0500

    fixed background. Connected buttons. Collected zip info.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,29 @@
+
+
+
 $(document).ready(() => {
     $('#search-btn').on('click', () => {
+        zipEntry = zip.value
+        $('#home').hide();
         $('#search-page').show();
+        console.log(zipEntry);
     })
 
     $('#find-me-btn').on('click', () => {
-
-    })
-
-    $('#go-back').on('click', () => {
         $('#home').hide();
         $('#search-page').show();
     })
 
-})
+    $('#go-back').on('click', () => {
+        $('#search-page').hide();
+        $('#home').show();
+    })
+
+});
+
+
+
+
+$(function(){
+$('#datepicker').datepicker();
+});

c7d4770b895c6ca3046011a886b8706ca026ae4b
Elena Carey <theelenacarey@gmail.com>
2022-06-15 15:56:42 -0500

    Working to make functions work together

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const NASA_API_KEY = "orR7avVLgSIHlctShMn825UHYNPINYB7iqOj2rAE"; // Terrible practice
 const GEO_API_KEY = "40604de622334a63bcab330aff218a62";
 
-
+//function to alert the zip code doesnt meet requirements
 function redAlert() {
     function undo(){
         $('#zip').removeClass('has-background-danger');
@@ -11,29 +11,37 @@ function redAlert() {
 }
 
 $(document).ready(() => {
+    //search button on click actions
     $('#search-btn').on('click', () => {
         zipEntry = zip.value;
         if (zipEntry.length > 4 && zipEntry.length < 12){
             $('#home').hide();
             $('#search-page').show();
-            console.log(zipEntry);
+            getLocationFromZip(zipEntry);
             zip.value = "";
         } else {
             redAlert();
         }
     })
 
+    //find my location button on click actions
     $('#find-me-btn').on('click', () => {
-        inSide = datepicker
         $('#home').hide();
         $('#search-page').show();
+        fetchLocalLocationInfo();
+        let info = fetchLocalLocationInfo().argument;
+        console.log(info);
+        
 
     })
 
+    //go back button
     $('#go-back').on('click', () => {
         $('#search-page').hide();
         $('#home').show();
     })
+
+    //Show new date button
     $('#show-img').on('click', () => {
         dayEntry = datepicker.value;
         console.log(dayEntry);
@@ -42,9 +50,7 @@ $(document).ready(() => {
 
 });
 
-
-
-
+//calendar function
 $(function(){
     $('#datepicker').datepicker({
         changeMonth: true,
@@ -70,7 +76,7 @@ function makeQueryString(paramsObject) {
 
 async function fetchLocalLocationInfo() {
     let fetchPromise = await fetch("http://ip-api.com/json/").then(response => response.json());
-    return {lon: fetchPromise.lon, lat: fetchPromise.lat}
+    return {lon: fetchPromise.lon, lat: fetchPromise.lat};
 }
 
 async function getLocationFromZip(query) {
@@ -99,9 +105,9 @@ async function requestBackupImage(lat, lon, dateString) {
 
 }
 
-$(async function () {
-    let localLocationInfo = await fetchLocalLocationInfo() // Used for "Find me" button
-    let locationInfo = await getLocationFromZip("11111")
+// $(async function () {
+//     let localLocationInfo = await fetchLocalLocationInfo() // Used for "Find me" button
+//     let locationInfo = await getLocationFromZip("11111")
 
-    let data = getRowAndColumn(30, -95) // Used for getting the nasa image
-})
+//     let data = getRowAndColumn(30, -95) // Used for getting the nasa image
+// })

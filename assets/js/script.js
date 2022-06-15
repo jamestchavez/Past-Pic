919b71ffc8e50f05a1df34df25f1d5fe1920b374
Elena Carey <theelenacarey@gmail.com>
2022-06-14 23:25:44 -0500

    merging

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+const NASA_API_KEY = "orR7avVLgSIHlctShMn825UHYNPINYB7iqOj2rAE"; // Terrible practice
+const GEO_API_KEY = "40604de622334a63bcab330aff218a62";
+
+
 function redAlert() {
     function undo(){
         $('#zip').removeClass('has-background-danger');
@@ -48,58 +52,56 @@ $(function(){
     });
 });
 
-// const NASA_API_KEY = "orR7avVLgSIHlctShMn825UHYNPINYB7iqOj2rAE"; // Terrible practice
-// const GEO_API_KEY = "40604de622334a63bcab330aff218a62";
 
-// // Makes query string from objject
-// function makeQueryString(paramsObject) {
-//     var rawQueryString = "?"; 
-//     var currentInteration = 0;
-//     let objectLength = Object.keys(paramsObject).length
-//     for (var key in paramsObject) {
-//         currentInteration += 1;
-//         rawQueryString = rawQueryString + key + "=" + paramsObject[key];
-//         if (currentInteration < objectLength) {
-//             rawQueryString = rawQueryString + "&"
-//         }
-//     }
-//     return encodeURI(rawQueryString);
-// }
+// Makes query string from object
+function makeQueryString(paramsObject) {
+    var rawQueryString = "?"; 
+    var currentIteration = 0;
+    let objectLength = Object.keys(paramsObject).length
+    for (var key in paramsObject) {
+        currentIteration += 1;
+        rawQueryString = rawQueryString + key + "=" + paramsObject[key];
+        if (currentIteration < objectLength) {
+            rawQueryString = rawQueryString + "&"
+        }
+    }
+    return encodeURI(rawQueryString);
+}
 
-// async function fetchLocalLocationInfo() {
-//     let fetchPromise = await fetch("http://ip-api.com/json/").then(response => response.json());
-//     return {lon: fetchPromise.lon, lat: fetchPromise.lat}
-// }
+async function fetchLocalLocationInfo() {
+    let fetchPromise = await fetch("http://ip-api.com/json/").then(response => response.json());
+    return {lon: fetchPromise.lon, lat: fetchPromise.lat}
+}
 
-// async function getLocationFromZip(query) {
-//     let data =  await fetch("https://api.geoapify.com/v1/geocode/search" + makeQueryString({apiKey: GEO_API_KEY, text : query})).then(response => response.json())
-//     if (data.features && data.features.length > 0) {
-//         var properties = data.features[0].properties
-//         return {lon: properties.lon, lat: properties.lat}
-//     } else {
-//         return {}
-//     }
-// }
+async function getLocationFromZip(query) {
+    let data =  await fetch("https://api.geoapify.com/v1/geocode/search" + makeQueryString({apiKey: GEO_API_KEY, text : query})).then(response => response.json())
+    if (data.features && data.features.length > 0) {
+        var properties = data.features[0].properties
+        return {lon: properties.lon, lat: properties.lat}
+    } else {
+        return {}
+    }
+}
 
 
-// async function requestImage(lat, lon, dateString) {
-//     let query = makeQueryString({
-//         api_key : NASA_API_KEY,
-//         lat : lat,
-//         lon : lon,
-//         date : dateString
-//     });
-//     return fetch("https://api.nasa.gov/planetary/earth/imagery" + query).then(response => response.json())
-// }
+async function requestImage(lat, lon, dateString) {
+    let query = makeQueryString({
+        api_key : NASA_API_KEY,
+        lat : lat,
+        lon : lon,
+        date : dateString
+    });
+    return fetch("https://api.nasa.gov/planetary/earth/imagery" + query).then(response => response.json())
+}
 
-// async function requestBackupImage(lat, lon, dateString) {
-//     return await "image.png"
+async function requestBackupImage(lat, lon, dateString) {
+    return await "image.png"
 
-// }
+}
 
-// $(async function () {
-//     let localLocationInfo = await fetchLocalLocationInfo() // Used for "Find me" button
-//     let locationInfo = await getLocationFromZip("11111")
+$(async function () {
+    let localLocationInfo = await fetchLocalLocationInfo() // Used for "Find me" button
+    let locationInfo = await getLocationFromZip("11111")
 
-//     let data = getRowAndColumn(30, -95) // Used for getting the nasa image
-// })
+    let data = getRowAndColumn(30, -95) // Used for getting the nasa image
+})
